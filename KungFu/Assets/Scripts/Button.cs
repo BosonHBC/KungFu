37c641b56ad612c534f5ae906c2bf1504f345825
BosonHBC <a923000213@163.com>
2019-01-13 21:26:24 -0700

    Initialize the whole project

---
+++ KungFu/Assets/Scripts/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+public class Button : MonoBehaviour {
+
+    [SerializeField] int ReadPin;
+    bool bBoardConnected = false;
+	// Use this for initialization
+	void Start () {
+        UduinoManager.Instance.pinMode(ReadPin, PinMode.Input_pullup);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int buttonValue = bBoardConnected?UduinoManager.Instance.digitalRead(ReadPin):-1;
+
+        if (buttonValue == 0)
+        {
+            Debug.Log("Down");
+        }
+        else if(buttonValue == 1)
+        {
+            Debug.Log("Up");
+        }
+
+    }
+
+    public void SetConnection(bool _connected)
+    {
+        bBoardConnected = _connected;
+
+    }
+}
---
+++ KungFu/Assets/Uduino/ReadMe.txt
@@ -0,0 +1,7 @@
+﻿Thanks for downloading Uduino !
+
+You can find more informations and tutorials on Uduino's webpage : 
+http://marcteyssier.com/uduino/
+
+IMPORTANT: 
+If you are Upgrading from Uduino 1.4.3 to Uduino 2.0.0+, please delete the /Uduino/ before importing the new one. 
