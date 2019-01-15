a590b1b2bd0e74f435b976fa489e1adb0b0db5d2
BosonHBC <a923000213@163.com>
2019-01-15 12:15:23 -0700

    DataNotFinished

---
+++ KungFu/Assets/Scripts/RythmPart/DataController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour
+{
+    // currently will be only one data
+    public MusicData[] allMusicData;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public MusicData GetCurrentMusicData()
+    {
+        return allMusicData[0];
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ KungFu/Assets/Scripts/RythmPart/MusicData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MusicData {
+    public string name;
+    public float lengthInSeconds;
+    public int numOfBeat;
+    public BeatData[] beatArray;
+
+
+}
