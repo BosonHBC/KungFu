f78de0eecead4c10678695f971f29b11d2f6ae9b
lopackuub <36348201+lopackuub@users.noreply.github.com>
2019-01-16 15:06:23 -0700

    Added the arduino sketch to the project

---
+++ KungFu/Assets/Scripts/ArduinoSketches/sketch_jan15a.ino
@@ -0,0 +1,158 @@
+const int button01 = 2;
+const int button02 = 3;
+const int button03 = 4;
+const int button04 = 5;
+const int button05 = 6;
+const int button06 = 7;
+const int button07 = 8;
+const int button08 = 9;
+const int button09 = 10;
+const int button10 = 11;
+const int button11 = 12;
+const int button12 = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(button01, INPUT);
+pinMode(button02, INPUT);
+pinMode(button03, INPUT);
+pinMode(button04, INPUT);
+pinMode(button05, INPUT);
+pinMode(button06, INPUT);
+pinMode(button07, INPUT);
+pinMode(button08, INPUT);
+pinMode(button09, INPUT);
+pinMode(button10, INPUT);
+pinMode(button11, INPUT);
+pinMode(button12, INPUT);
+
+digitalWrite(button01, HIGH);
+digitalWrite(button02, HIGH);
+digitalWrite(button03, HIGH);
+digitalWrite(button04, HIGH);
+digitalWrite(button05, HIGH);
+digitalWrite(button06, HIGH);
+digitalWrite(button07, HIGH);
+digitalWrite(button08, HIGH);
+digitalWrite(button09, HIGH);
+digitalWrite(button10, HIGH);
+digitalWrite(button11, HIGH);
+digitalWrite(button12, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  String myString = "";
+  if(digitalRead(button01) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button01) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button02) == LOW)
+ {
+    myString += "1";
+  }
+  else if(digitalRead(button02) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button03) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button03) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button04) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button04) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button05) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button05) == HIGH)
+  {
+    myString += "0";
+  }
+  
+  if(digitalRead(button06) == LOW)
+ {
+    myString += "1";
+  }
+  else if(digitalRead(button06) == HIGH)
+  {
+    myString += "0";
+  }
+  
+  if(digitalRead(button07) == LOW)
+ {
+    myString += "1";
+  }
+  else if(digitalRead(button07) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button08) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button08) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button09) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button09) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button10) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button10) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button11) == LOW)
+ {
+    myString += "1";
+  }
+  else if(digitalRead(button11) == HIGH)
+  {
+    myString += "0";
+  }
+
+  if(digitalRead(button12) == LOW)
+  {
+    myString += "1";
+  }
+  else if(digitalRead(button12) == HIGH)
+  {
+    myString += "0";
+  }
+    Serial.println(myString);
+    Serial.flush();
+    delay(250);
+}
