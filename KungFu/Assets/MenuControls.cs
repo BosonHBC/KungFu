fcc9b8263b88edd0c1ec886056dc02c5084640a9
lopackuub <36348201+lopackuub@users.noreply.github.com>
2019-01-16 15:00:07 -0700

    Multiple button inputs working
    Arduino now sending output of buttons as a string of 1s and 0s. ArduinoInputScript translates the string and saves it to an array of bools.
    Going to start work on the menu controls now.

---
+++ KungFu/Assets/MenuControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour
+{
+    GameObject input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GameObject.Find("UdinoManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ KungFu/Assets/Scripts/ArduinoInputScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+using Uduino;
+
+public class ArduinoInputScript : MonoBehaviour {
+
+    internal bool[] buttons = new bool[12];
+    private int NUMBUTTONS = 12;
+    private string outputString;
+    
+    SerialPort sp = new SerialPort("COM3",9600); 
+
+	// Use this for initialization
+	void Start () {
+        //opens the port and sets the read timeout. The timeout should match the write timeout in the arduino sketch.
+        sp.Open();
+        sp.ReadTimeout = 250;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if port is open and read the string from arduino.
+        if(sp.IsOpen)
+        {
+            try
+            {
+               outputString = sp.ReadLine();
+            }
+            catch(System.Exception)
+            {
+                
+            }
+        }
+        //Transfers the info sent by arduino to the bool array of button inputs.
+        stringToBoolArray(outputString);
+    }
+
+    void stringToBoolArray(string input)
+    {
+        for(int i = 0; i < NUMBUTTONS; i++)
+        {
+            if (input.ToCharArray()[i] == '1')
+            {
+                buttons[i] = true;
+            }
+            else
+            {
+                buttons[i] = false;
+            }
+        }
+        Debug.Log(input + "\n");
+    }
+}
