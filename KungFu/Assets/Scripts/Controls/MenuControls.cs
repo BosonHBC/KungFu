224a36bdc713ab786c5cbf75129fe319e23ea600
lopackuub <36348201+lopackuub@users.noreply.github.com>
2019-01-17 11:05:25 -0700

    prefab changed and started menu controls

--- KungFu/Assets/Scripts/Controls/MenuControls.cs
+++ KungFu/Assets/Scripts/Controls/MenuControls.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MenuControls : MonoBehaviour
 {
-    GameObject input;
+    private GameObject input;
+    private int currentSelectedID;
+    public Button[] buttonObjects;
+    public int enterID;
+    public int backID;
+    public int upID;
+    public int downID;
 
     // Start is called before the first frame update
     void Start()
     {
         input = GameObject.Find("UdinoManager");
+
+        //The starting button should be set as the first gameobject in the array and should be highlighted in the editor as well
+        currentSelectedID = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+       if(input.GetComponent<ArduinoInputScript>().buttons[upID])
+        {
+            if(currentSelectedID != 0)
+            {
+                currentSelectedID--;
+                buttonObjects[currentSelectedID].Select();
+            }
+        }
     }
 }
