89e03b131618d0eb8107f062a3590cd279b671a4
lopackuub <36348201+lopackuub@users.noreply.github.com>
2019-01-22 11:12:09 -0700

    Added sound effects to project. Sound effects play wjhen navigating the menu

--- KungFu/Assets/Scripts/Controls/MenuControls.cs
+++ KungFu/Assets/Scripts/Controls/MenuControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class MenuControls : MonoBehaviour
 {
+    private AudioSource source;
     private bool canRegister;
     private GameObject input;
     private int currentSelectedID;
@@ -13,13 +14,15 @@ public class MenuControls : MonoBehaviour
     public int backID;
     public int upID;
     public int downID;
+    public AudioClip gong;
+    public AudioClip[] whoosh;
 
     // Start is called before the first frame update
     void Start()
     {
         canRegister = true;
         input = GameObject.Find("UduinoManager");
-
+        source = gameObject.GetComponent<AudioSource>();
         //The starting button should be set as the first gameobject in the array and should be highlighted in the editor as well
         currentSelectedID = 0;
     }
@@ -33,6 +36,7 @@ public class MenuControls : MonoBehaviour
             //Select above button trigger with wrap around
             if (input.GetComponent<ArduinoInputScript>().buttons[upID] && !input.GetComponent<ArduinoInputScript>().buttons[downID])
             {
+                source.PlayOneShot(whoosh[Random.Range(0,whoosh.Length)]);
                 if (currentSelectedID != 0)
                 {
                     currentSelectedID--;
@@ -48,6 +52,7 @@ public class MenuControls : MonoBehaviour
             //Select below button trigger with wrap around
             if (input.GetComponent<ArduinoInputScript>().buttons[downID] && !input.GetComponent<ArduinoInputScript>().buttons[upID])
             {
+                source.PlayOneShot(whoosh[Random.Range(0, whoosh.Length)]);
                 if (currentSelectedID != buttonObjects.Length - 1)
                 {
                     currentSelectedID++;
@@ -63,12 +68,14 @@ public class MenuControls : MonoBehaviour
             //Submit trigger
             if (input.GetComponent<ArduinoInputScript>().buttons[enterID]  && !input.GetComponent<ArduinoInputScript>().buttons[downID] && !input.GetComponent<ArduinoInputScript>().buttons[upID])
             {
+                source.PlayOneShot(gong);
                 buttonObjects[currentSelectedID].onClick.Invoke();
             }
 
             //Close menu trigger
             if (input.GetComponent<ArduinoInputScript>().buttons[backID] && !input.GetComponent<ArduinoInputScript>().buttons[downID] && !input.GetComponent<ArduinoInputScript>().buttons[upID])
             {
+                source.PlayOneShot(gong);
                 this.gameObject.SetActive(false);
             }
         }
