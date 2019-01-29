dc2e81007aa8559552bd61fc08f2cbf9a22985ba
lopackuub <36348201+lopackuub@users.noreply.github.com>
2019-01-29 09:37:46 -0700

    Imported leaderboard script

---
+++ KungFu/Assets/Scripts/UI/GetPlayerNameScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPlayerNameScript : MonoBehaviour {
+
+    internal string stringToEdit;
+    private AudioSource audioSource;
+    public AudioClip highScoreSound;
+
+    private void Awake()
+    {     
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        stringToEdit = " HighScore!!! Please Enter Your Name";
+        audioSource.PlayOneShot(highScoreSound);
+    }
+
+    void OnGUI()
+    {
+        stringToEdit = GUI.TextField(new Rect(Screen.width/2-365, Screen.height/2+165, 730, 50), stringToEdit, 50);
+        GUI.skin.textField.fontSize = 40;
+    }
+}
