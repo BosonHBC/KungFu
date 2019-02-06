be4a561aabeb38745c53143205835f323e65eb55
Byreave Luo <654297651@qq.com>
2019-02-05 18:19:46 -0700

    add whitebox

---
+++ KungFu/Assets/Scripts/ByreaveTestScripts/TestDataLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TestDataLoader : MonoBehaviour
+{
+    // currently will be only one data
+    private MusicData[] allMusicData;
+    private string gameDataFileName = "byreaveTest.json";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+        if (File.Exists(filePath))
+        {
+            string dataJson = File.ReadAllText(filePath);
+
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataJson);
+
+            allMusicData = loadedData.allMusicData;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+}
