5315133ea1d53d299465e193d58313c2d5c2ac53
Boson <bosonhuang22@gmail.com>
2019-02-15 20:32:15 -0800

    Import Cinemachine Install and Test scene Implemented

---
+++ KungFu/Assets/Scripts/Fighting/PlayerCenter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCenter : MonoBehaviour
+{
+    [SerializeField] Transform trP1;
+    [SerializeField] Transform trP2;
+
+    [SerializeField] float fLerpTime;
+    [SerializeField] float fYOffset; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, CenterPoint() + new Vector3(0, fYOffset,0), fLerpTime);
+    }
+
+    Vector3 CenterPoint()
+    {
+        return (trP1.position + trP2.position) / 2;
+    }
+    
+}
