0738b87181fc6ece4d56e126fd9d9372d8502459
lopackuub <36348201+lopackuub@users.noreply.github.com>
2019-02-21 13:53:41 -0700

    Getting input from 2 arduinos

---
+++ KungFu/Assets/Scripts/ArduinoSketches/sketch_feb12_other/sketch_feb12_other.ino
@@ -0,0 +1,72 @@
+/* FSR simple testing sketch.  <br>Connect one end of FSR to power, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground 
+*/
+const int button01 = 0; // the FSR and 10K pulldown are connected to a0
+const int button02 = 1;
+const int button03 = 2;
+const int button04 = 3;
+const int button05 = 4;
+const int button06 = 5;
+
+const int ThreshHold = 600;
+ 
+void setup(void) {
+  Serial.begin(9600);   
+}
+ 
+void loop(void) {
+    String myString = ""; 
+    
+    // the analog reading from the FSR resistor divider
+  if (analogRead(button01) >= ThreshHold) {  
+    myString += "1";
+  }
+  else
+  {
+    myString += "0";
+  }
+
+  if (analogRead(button02) >= ThreshHold) {  
+    myString += "1";
+  }
+  else
+  {
+    myString += "0";
+  }
+
+  if (analogRead(button03) >= ThreshHold) {  
+    myString += "1";
+  }
+  else
+  {
+    myString += "0";
+  }
+
+  if (analogRead(button04) >= ThreshHold) {  
+    myString += "1";
+  }
+  else
+  {
+    myString += "0";
+  }
+
+  if (analogRead(button05) >= ThreshHold) {  
+    myString += "1";
+  }
+  else
+  {
+    myString += "0";
+  }
+
+   if (analogRead(button06) >= ThreshHold) {  
+    myString += "1";
+  }
+  else
+  {
+    myString += "0";
+  }
+ 
+  Serial.println(myString);
+  Serial.flush();
+  delay(5);
+}
--- KungFu/Assets/Scripts/Controls/ArduinoInputScript.cs
+++ KungFu/Assets/Scripts/Controls/ArduinoInputScript.cs
@@ -9,36 +9,54 @@ public class ArduinoInputScript : MonoBehaviour
 
     internal bool[] buttons = new bool[11];
     private int NUMBUTTONS = 11;
-    private string outputString;
+    private string outputStringCom3;
+    private string outputStringCom4;
 
     [SerializeField] bool bDebugPrintInput;
 
-    SerialPort sp = new SerialPort("COM3", 9600);
+    SerialPort com3 = new SerialPort("COM3", 9600);
+    SerialPort com4 = new SerialPort("COM4", 9600);
 
     // Use this for initialization
     void Start()
     {
         //opens the port and sets the read timeout. The timeout should match the write timeout in the arduino sketch to remove data stream lag.
-        sp.Open();
-        sp.ReadTimeout = 5;
+        com3.Open();
+        com3.ReadTimeout = 5;
+        com4.Open();
+        com4.ReadTimeout = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
         //check if port is open and read the string from arduino.
-        if (sp.IsOpen)
+        if (com3.IsOpen)
         {
             try
             {
-                outputString = sp.ReadLine();
+                outputStringCom3 = com3.ReadLine();
             }
             catch (System.Exception)
             {
                 //Debug.Log("Port Closed");
             }
         }
-        //Transfers the info sent by arduino to the bool array of button inputs.
+
+        if (com4.IsOpen)
+        {
+            try
+            {
+                outputStringCom4 = com4.ReadLine();
+            }
+            catch (System.Exception)
+            {
+                //Debug.Log("Port Closed");
+            }
+        }
+
+        //Transfers the info sent by arduinos to the bool array of button inputs.
+        string outputString = outputStringCom3 + outputStringCom4;
         stringToBoolArray(outputString);
     }
 
@@ -66,12 +84,6 @@ public class ArduinoInputScript : MonoBehaviour
         }
         if (bDebugPrintInput)
             Debug.Log(input + "\n");
-
-        //for (int i = 0; i < buttons.Length; i++)
-        //{
-        //    if (buttons[i])
-        //        Debug.Log("Button " + i + "is pressed");
-        //}
 
         //Saves input to GameManager
         if (GameObject.Find("GameManager") != null)
