cc7bff459df4f57ebdbe3e2eef1060bfcf6a5af5
Boson <bosonhuang22@gmail.com>
2019-04-01 14:48:47 -0700

    New Light map and Prepare Fight lerp

--- KungFu/Assets/Scripts/Animation/PrepareSmb.cs
+++ KungFu/Assets/Scripts/Animation/PrepareSmb.cs
@@ -6,16 +6,16 @@ public class PrepareSmb : BaseSmb
 {
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        base.OnStateEnter(animator, stateInfo, layerIndex);        
-
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        BaseAnimController baseAnim =
+animator.gameObject.GetComponent<BaseAnimController>();
+        baseAnim.LerpFromPrepareToFight();
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateExit(animator, stateInfo, layerIndex);
-        BaseAnimController baseAnim =
-        animator.gameObject.GetComponent<BaseAnimController>();
-        baseAnim.LerpFromPrepareToFight();
+
     }
 
 }
