cbeda504e698bb37dfbb7f48559523a52e892bab
LuisGarciaRemes <36348201+lopackuub@users.noreply.github.com>
2019-04-09 09:48:16 -0600

    last fix

--- KungFu/Assets/Scripts/ArduinoSketches/sketch_jan15a/sketch_jan15a.ino
+++ KungFu/Assets/Scripts/ArduinoSketches/sketch_jan15a/sketch_jan15a.ino
@@ -95,7 +95,7 @@ void loop(void) {
     myString += "0";
   }
 
-  if (analogRead(button10) >= ThreshHold) {
+  if (analogRead(button10) >= ThreshHold - 100) {
     myString += "1";
   }
   else
@@ -103,7 +103,7 @@ void loop(void) {
     myString += "0";
   }
 
-  if (analogRead(button11) >= ThreshHold - 100) {
+  if (analogRead(button11) >= ThreshHold) {
     myString += "1";
   }
   else
