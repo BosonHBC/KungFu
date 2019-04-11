beb08e5a92d2c4f669e662c386f237927f929a20
Boson <bosonhuang22@gmail.com>
2019-04-10 17:26:00 -0700

    EndUIAlmost Finish

---
+++ KungFu/Assets/Scripts/UI/EndUI/EndUIController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EndUIController : MonoBehaviour
+{
+    Animator anim;
+    AnimationClip clip;
+    AnimationEvent fillEvent;
+    float[] gradeOfScore = { 0.4f, 0.6f, 0.8f };
+    int iMaxScore;
+    [SerializeField] Sprite[] gradeImgs;
+    [SerializeField] Image fillImg;
+    private float fillDuration = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        fillEvent = new AnimationEvent();
+        fillEvent.time = 140f / 60f;
+        fillEvent.functionName = "FillImage";
+        fillEvent.floatParameter = 0.8f;
+        clip = anim.runtimeAnimatorController.animationClips[0];
+        clip.AddEvent(fillEvent);
+
+        
+        anim.Play("EndUI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartEndSession(int _numOfBeat, int _PerfectScore, int _comboAward)
+    {
+        anim.Play("EndUI");
+        
+        // Calculate Max score
+        iMaxScore = 0;
+        for (int i = 0; i < _numOfBeat; i++)
+        {
+            iMaxScore += _PerfectScore + i * _comboAward;
+        }
+    }
+
+    public void SetData(int _maxCombo, int _score,  float _p1RemainHp, float _p2RemainHp)
+    {
+        float percentage = _score / iMaxScore;
+        fillEvent.floatParameter = percentage;
+    }
+
+    public void FillImage(float _percent = 0.8f)
+    {
+        StartCoroutine(ie_FillImage(_percent));
+    }
+
+    IEnumerator ie_FillImage(float _end)
+    {
+        float _timeStartFade = Time.time;
+        float _timeSinceStart = Time.time - _timeStartFade;
+        float _lerpPercentage = _timeSinceStart / fillDuration;
+
+        while (true)
+        {
+            _timeSinceStart = Time.time - _timeStartFade;
+            _lerpPercentage = _timeSinceStart / fillDuration;
+
+            float currentValue = Mathf.Lerp(0, _end, _lerpPercentage);
+            fillImg.fillAmount = currentValue;
+
+            if (_lerpPercentage >= 1) break;
+
+
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+}
---
+++ KungFu/Assets/Scripts/UI/EndUI/InputNameControl.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputNameControl : MonoBehaviour
+{
+    public bool bCanShoose;
+    private int iMaxIndex = 3;
+    private int iCurrentIndex;
+    private int[] initials = { 0, 0, 0 };
+    char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    Text[] childs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        childs = new Text[] {
+            transform.GetChild(1).GetChild(2).GetComponent<Text >(),
+            transform.GetChild(2).GetChild(2).GetComponent<Text >(),
+            transform.GetChild(3).GetChild(2).GetComponent<Text >()
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DebugTest();
+    }
+    private void DebugTest()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            NextCharacter();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            PreviousCharacter();
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            NextIndex();
+
+    }
+
+    public void NextIndex()
+    {
+        if (bCanShoose)
+        {
+            iCurrentIndex++;
+            if (iCurrentIndex < iMaxIndex)
+            {
+                childs[iCurrentIndex - 1].transform.parent.GetChild(0).gameObject.SetActive(false);
+                childs[iCurrentIndex - 1].transform.parent.GetChild(1).gameObject.SetActive(false);
+
+                childs[iCurrentIndex].transform.parent.GetChild(0).gameObject.SetActive(true);
+                childs[iCurrentIndex].transform.parent.GetChild(1).gameObject.SetActive(true);
+            }
+            else
+            {
+                bCanShoose = false;
+                childs[iMaxIndex - 1].transform.parent.GetChild(0).gameObject.SetActive(false);
+                childs[iMaxIndex - 1].transform.parent.GetChild(1).gameObject.SetActive(false);
+                // Upload Data to the High Score Manager
+            }
+        }
+       
+    }
+
+    public void NextCharacter()
+    {
+        if (bCanShoose && iCurrentIndex < iMaxIndex)
+        {
+            initials[iCurrentIndex]++;
+            if (initials[iCurrentIndex] > 25)
+                initials[iCurrentIndex] = 0;
+
+            childs[iCurrentIndex].text = alpha[initials[iCurrentIndex]].ToString() + ".";
+        }
+    }
+    public void PreviousCharacter()
+    {
+        if (bCanShoose && iCurrentIndex < iMaxIndex)
+        {
+            initials[iCurrentIndex]--;
+            if (initials[iCurrentIndex] < 0)
+                initials[iCurrentIndex] = 25;
+
+            childs[iCurrentIndex].text = alpha[initials[iCurrentIndex]].ToString() + ".";
+        }
+    }
+}
