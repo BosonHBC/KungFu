f47df101522f6aeab8a9b1623a2c1c95acd2a43c
Byreave Luo <654297651@qq.com>
2019-04-17 14:31:14 -0600

    add credits and main menu and transitions

---
+++ KungFu/Assets/Scripts/UI/MenuUIScripts/MenuCanvasControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCanvasControl : MonoBehaviour
+{
+    TransitionControl CanvasMove;
+    //0 Main, 1 Song Select, 2 Character Select, 3 Credits
+    public int CurrentCanvas = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasMove = FindObjectOfType<TransitionControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputFromKeyboard();
+    }
+    void CheckInputFromKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CanvasMove.MoveMenu(-1);
+            CurrentCanvas -= 1;
+            if (CurrentCanvas < 0)
+                CurrentCanvas = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            CanvasMove.MoveMenu(1);
+            CurrentCanvas = (CurrentCanvas + 1) % 4;
+        }
+    }
+}
---
+++ KungFu/Assets/Scripts/UI/MenuUIScripts/TipsScroller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipsScroller : MonoBehaviour
+{
+    public Transform TipParent;
+    [SerializeField]
+    float TipShowTime = 3.0f;
+    Text[] Tips;
+    int currentTip = 0;
+    float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tips = TipParent.GetComponentsInChildren<Text>();
+        for(int i = 1; i < Tips.Length; ++ i)
+        {
+            Tips[i].canvasRenderer.SetAlpha(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer >= TipShowTime)
+        {
+            ShowNextTip();
+            timer = 0.0f;
+        }
+        timer += Time.deltaTime;
+    }
+
+    void ShowNextTip()
+    {
+        int nextTipIndex = (currentTip + 1) % Tips.Length;
+        StartCoroutine(TipFadeOut(currentTip));
+        Tips[nextTipIndex].CrossFadeAlpha(1.0f, 1.0f, false);
+        currentTip = nextTipIndex;
+    }
+
+    IEnumerator TipFadeOut(int index, float fadeTime = 1.0f)
+    {
+        Text tipToFade = Tips[index];
+        Vector2 destination = tipToFade.rectTransform.anchoredPosition + new Vector2(0, 100.0f);
+        Vector2 speed = (destination - tipToFade.rectTransform.anchoredPosition) / fadeTime;
+        tipToFade.CrossFadeAlpha(0.0f, fadeTime, false);
+        var wait = new WaitForEndOfFrame();
+        while(tipToFade.rectTransform.anchoredPosition.y < destination.y)
+        {
+            tipToFade.rectTransform.anchoredPosition += speed * Time.deltaTime;
+            yield return wait;
+        }
+        tipToFade.canvasRenderer.SetAlpha(0.0f);
+        tipToFade.rectTransform.anchoredPosition = Vector2.zero;
+    }
+}
