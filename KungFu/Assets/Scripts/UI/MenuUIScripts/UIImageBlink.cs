92ca1013231f1216f9fc23da8b710a2cd83f4a61
Byreave Luo <654297651@qq.com>
2019-04-16 20:17:10 -0600

    Add Credits and unfinished Main Menu

---
+++ KungFu/Assets/Scripts/UI/MenuUIScripts/UIImageBlink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIImageBlink : MonoBehaviour
+{
+    public Image ImageToBlink;
+    public float BlinkInterval;
+    [Range(0.0f, 1.0f)]
+    public float MinAlpha;
+    [Range(0.5f, 1.0f)]
+    public float MaxAlpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ImageToBlink.canvasRenderer.SetAlpha(MaxAlpha);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ImageToBlink.canvasRenderer.GetAlpha() >= MaxAlpha)
+        {
+            ImageToBlink.CrossFadeAlpha(MinAlpha, BlinkInterval, false);
+        }
+        else if(ImageToBlink.canvasRenderer.GetAlpha() <= MinAlpha)
+        {
+            ImageToBlink.CrossFadeAlpha(MaxAlpha, BlinkInterval, false);
+        }
+    }
+}
