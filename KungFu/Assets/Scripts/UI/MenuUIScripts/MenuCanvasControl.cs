2b66d401fe87de02246bbfc6a73525e1447c971d
Byreave Luo <654297651@qq.com>
2019-04-17 16:06:54 -0600

    add music in menus and try to fix sfx in fighting scene

--- KungFu/Assets/Scripts/UI/MenuUIScripts/MenuCanvasControl.cs
+++ KungFu/Assets/Scripts/UI/MenuUIScripts/MenuCanvasControl.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class MenuCanvasControl : MonoBehaviour
 {
     TransitionControl CanvasMove;
+    AudioSource audioSource;
     //0 Main, 1 Song Select, 2 Character Select, 3 Credits
     public int CurrentCanvas = 0;
+    public AudioClip CanvasChange;
+    public AudioClip GameStart;
     // Start is called before the first frame update
     void Start()
     {
         CanvasMove = FindObjectOfType<TransitionControl>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,11 +30,13 @@ public class MenuCanvasControl : MonoBehaviour
             CurrentCanvas -= 1;
             if (CurrentCanvas < 0)
                 CurrentCanvas = 3;
+            audioSource.PlayOneShot(CanvasChange);
         }
         if (Input.GetKeyDown(KeyCode.Q))
         {
             CanvasMove.MoveMenu(1);
             CurrentCanvas = (CurrentCanvas + 1) % 4;
+            audioSource.PlayOneShot(CanvasChange);
         }
     }
 }
